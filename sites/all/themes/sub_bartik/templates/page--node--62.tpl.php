fecd805686b70660cd0dab21d4c7ebb054ecb394
mbp <talavishay@gmail.com>
2013-05-22 12:34:19 +0300

    facebook v1.2

--- sites/all/themes/sub_bartik/templates/page--node--62.tpl.php
+++ sites/all/themes/sub_bartik/templates/page--node--62.tpl.php
@@ -107,8 +107,9 @@
 <? else:?>
 <? 
 //print sub_bartik_fboauth_action__connect();
-$link = fboauth_action_link_properties('connect');
-print l(t('Connect'), $link['href'], array('query' => $link['query']));
+//$link = fboauth_action_link_properties('connect', "http://jukespot.tk/start");
+//print l(t('Connect'), $link['href'], array('query' => $link['query']));
+print fboauth_action_display("connect");
 ?> 
 <?endif;?>
 </div>
