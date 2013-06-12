f5af9727de45c4623eb342b028b9885dd34fa382
mbp <talavishay@gmail.com>
2013-06-13 00:23:01 +0300

    results V0.2

--- sites/all/modules/mimi/results.js
+++ sites/all/modules/mimi/results.js
@@ -1,50 +1,101 @@
 
 jQuery(document).ready(function(){
+    scale("mono");
     mimi = {};
 mimi.color = new Array("#33cccc","#ffff00","#FF6699","#00ff00");
-mimi.counter = 10;
-    refresh();    
+mimi.counter = 1;
+    start();    
 //window.setInterval(function(){
 //    refresh();    
 //},7000);
 
 });
-function refresh(){
-jQuery(".content").html("");
+function start(){
+    jQuery(".content").html("");
     jQuery.ajax({
         type: "GET",
         url: '/get_results' ,
         ifModified : true,
-        beforeSend: function(){
-  
-        },
-        complete: function(){
-  
-        },
-
+        beforeSend: function(){        },
+        complete: function(){        },
         success: function(data){
+                    
+                    mimi.runtime = data.runtime;
+                    mimi.time = data.time;
+                    mimi.deadline = mimi.time +mimi.runtime ;
+                    delete data.time;
+                    delete data.runtime;
+                    mimi.respondData = data;
+                     var dataArray = new Array();
+                jQuery.each(data,function(key, val){
+                   
+                    dataArray.push(val.votes);
+                });
+                mimi.winner = Math.max.apply(Math, dataArray); // 306
+                jQuery.each(data,function(i, option){
+                    jQuery.each(option,function(key, val){
+                    if(key === "votes" && mimi.winner === parseInt(val)){
+                        mimi.winner = data[i];
+                        mimi.winner.color = mimi.color[i]; 
+                    };
+                });
+                });
+countdown();
+//refresh();
+        },
+        dataType: "json"
+    });
+    
+};
+function refresh(){
+    jQuery("#countdown").remove();
+            var data = mimi.respondData;
             arrayOfData1 = new Array();
             mimi.data = new Array();
-            jQuery.each(data ,function(key, val){
-                counter = parseInt(key)+1;
-      //          jQuery(".content").append(val.title+"<br/>"+val.percentage+"%");
+            var i =0;
             
-            arrayOfData1.push([1, val.title, mimi.color[counter-1]]);
-            
-            mimi.data.push([val.percentage, val.title, mimi.color[counter-1]]);
-            
+            jQuery.each(data,function(key, val){
+                arrayOfData1.push([1, val.title, mimi.color[i]]);            
+                mimi.data.push([val.percentage, val.title, mimi.color[i]]);            
+                
+                i++;
             });
             jQuery('.content').jqBarGraph({  data: arrayOfData1 ,colors: mimi.color ,sort:false,speed:0.1, width:jQuery(window).width()-100,height:220});
             jQuery('.content').append(jQuery('<div id="counter"><span>The<br/>Winner<br/>Is</span></div>'));
-            window.setTimeout(function(){
-            jQuery('#couner span').remove();    
-            
+             window.setTimeout(function(){
+            jQuery('#couner span').remove();
+
             mimi.timer = window.setInterval(function(){
-                
+
                 jQuery("#counter").text(mimi.counter);
                 if(mimi.counter === 0){
                     jQuery(".content").html("");
-                    jQuery('.content').jqBarGraph({ data: mimi.data, colors: mimi.color, sort:false,speed:3, width:jQuery(window).width()-100,height:window.innerHeight-100});
+                    mimi.timer2 = window.setTimeout(function(){
+                        jQuery(".content").html("");
+                        jQuery(".circle-container").css({"width":"100%","height":"100%"}).show();
+                        jQuery(".circle a").append('<span class="word">'+mimi.winner.title.replace(/ /i,'</span><br/><span class="word">')+'</span>').show();
+                        jQuery(".circle").css({"border-color":mimi.winner.color}).show().children("a").css({ "display": "block", "width": "100%","color":mimi.winner.color}).show();
+                        fitTextInBox(jQuery(".circle a"));
+                        jQuery(".fix").not(".word").remove();
+                        var circleTextHeight = jQuery('.circle a').height();
+                        var circleHeight = jQuery('.circle a').parent().height();
+                        var circleMargin =circleHeight-circleTextHeight;
+                        jQuery('.circle a').css("margin-top", circleMargin/2);
+                        var circleTextHeight = jQuery('#container').height();
+                        var circleHeight = jQuery(window).height();
+                        var circleMargin =circleHeight-circleTextHeight;
+                        jQuery('#container').css("top", circleMargin*0.5);
+                        jQuery(".circle").css({
+                            "animation-delay":"0", "animation-duration": ".5s", "animation-iteration-count": "12",    "animation-name": "pulseScale"  });
+                    },4500);
+                    jQuery('.content').jqBarGraph({
+                                                data: mimi.data, 
+                                                colors: mimi.color, 
+                                                sort:false,
+                                                speed:4.5, 
+                                                width:jQuery(window).width()-100,
+                                                height:window.innerHeight-100
+                    });
                     delete mimi.data;
                     clearInterval(mimi.timer);
                 }
@@ -52,9 +103,196 @@ jQuery(".content").html("");
             },1000);
             delete arrayOfData1 ;
             }, 2000);
+
             
-        },
-        dataType: "json"
+//                window.setTimeout(function(){
+//    //                        dream();
+//                },3000);
+}
+function countdown(){
+    jQuery('.content').prepend(jQuery('<div id="countdown"><div id="time_left_text">הזמן הנותר להצבעה</div><div class="kkcount-down" data-time="'+mimi.deadline+'"></div></div>'));
+    jQuery(".kkcount-down").kkcountdown({
+        dayText : 'day ',
+        daysText : 'days ',
+        hoursText : '',
+        minutesText : ':',
+        secondsText : '',
+        displayZeroDays : false,
+        oneDayClass : 'one-day',
+        callback: refresh
     });
-    
 };
+function dream(){
+
+var color = Math.round(0xffffff * Math.random()).toString(16);
+
+//alert(color);
+
+var x = Math.floor(Math.random()*jQuery(window).width());
+
+var y = Math.floor(Math.random()*jQuery(window).height());
+
+//alert(x+"--"+y);
+
+drawingpix = jQuery('<span>').attr({class: 'drawingpix'}).hide();
+
+							 jQuery(document.body).append(drawingpix);
+
+							 drawingpix.css({
+
+											'background-color':color,
+
+											'border-radius':'100px',
+
+											'-moz-border-radius': '100px',
+
+											'-webkit-border-radius': '100px',
+
+											'z-index':-1,
+
+											top: y-14,	//offsets
+
+											left: x-14 //offsets
+
+											}).show().animate({
+
+																height:'500px',
+
+																width:'500px',
+
+																'border-radius':'500px',
+
+																'-moz-border-radius': '500px',
+
+																'-webkit-border-radius': '500px',
+
+																opacity: 0.1,
+
+																top: y-250,	//offsets
+
+																left: x-250
+
+						   
+
+							}, 5000).fadeOut(2000);
+
+							 window.setTimeout('dream()',2000);
+
+							
+
+
+}
+function monoCircle(e) {
+    
+    jQuery("body").removeClass("multi").addClass("mono");
+    jQuery(".circle a").hide();
+    jQuery(e.currentTarget).css("width","82%").parent(".circle-container").addClass("current");
+    jQuery('.circle-container:not(.current)').fadeOut();
+    scale("mono");
+    jQuery(e.currentTarget).parent(".circle-container").animate({"width": "100%", "height": "100%", "opacity": 1}, function() {
+         jQuery("#bottom").show();
+//        var circleSize = jQuery(".circle", this).height();
+//        var fontSize = circleSize / 3;
+//        var marginTop = fontSize - (fontSize * 0.2);
+//         jQuery("a", e.currentTarget).css({"line-height": "normal","height":"auto","padding-top":"0px","font-size":"inherit"});
+        
+        if(typeof(e) !== "undefined"){
+            jQuery("a", e.currentTarget).css("opacity", 0);
+            jQuery("a", e.currentTarget).fadeIn("fast",function(){
+                    fitTextInBox(jQuery("a", e.currentTarget));
+                     var val = jQuery("a", e.currentTarget);
+                    var circleTextHeight = jQuery(val).height();
+                    var circleHeight = jQuery(val).parent().height();
+                    var circleMargin =circleHeight-circleTextHeight;
+                    jQuery(val).css("margin-top", circleMargin/2);
+                    jQuery("a", e.currentTarget).css("opacity", 1);
+            });
+        }
+            
+    });
+}
+function scale(mono) {
+
+    fullWidth =  jQuery(window).width() ;
+    fullHeight =  window.innerHeight;
+   
+    jQuery("body").css({"min-height":fullHeight-20 });
+    if (fullWidth > fullHeight) {
+        jQuery("body").addClass("horiz").removeClass("verti");
+        var size = fullHeight;
+        var horizMargin = ((fullWidth - fullHeight) / 2);
+        if(horizMargin > 150){
+            jQuery("#logo").css({"width": horizMargin, "height": horizMargin});
+            jQuery("#logo_container").css({"height": horizMargin});
+        }
+    }
+    if (fullHeight > fullWidth) {
+        jQuery("body").addClass("verti").removeClass("horiz");
+        var size = fullWidth;
+        var verticalMargin = (fullHeight - fullWidth) * 0.6;
+        if(verticalMargin > 150){
+            jQuery("#logo").css({"width": verticalMargin, "height": verticalMargin});
+            jQuery("#logo_container").css({"height": verticalMargin});
+        }
+    }
+    //scale down container for the logo
+//    size = (size / 100) * 75;
+    if(mono === "mono"){
+        size = (size / 100) * 70;
+    }
+    var containter_margin_top  = (window.innerHeight-size ) / 2 ;
+size = size *1.1;
+//    jQuery("#container").css({"width": size, "height": size, "margin-top": containter_margin_top  });//.text(fullWidth).css("font-size","50px");
+    jQuery("#container").css({"width": size, "height": size});//.text(fullWidth).css("font-size","50px");
+
+        ;
+        
+    
+}
+
+	function fitTextInBox(fitTextInBox_currentBox, maxHeight){
+            Drupal.fitTextInBox = {};
+            if(jQuery("a", fitTextInBox_currentBox).length === 1){
+                fitTextInBox_currentBox = jQuery("a", fitTextInBox_currentBox);
+            }
+// var fitTextInBox_maxWidth = false;//	var fitTextInBox_currentWidth = false;//	var fitTextInBox_currentBox = false;//	var fitTextInBox_currentTextObj = false;
+	var fitTextInBox_maxHeight = false;
+        if(maxHeight){
+            fitTextInBox_maxHeight=maxHeight;
+        } else {
+            fitTextInBox_maxHeight = 10000;
+        }
+        
+//        var fitTextInBox_currentBox= jQuery("a", e.currentTarget);
+        Drupal.fitTextInBox.maxWidth = jQuery(fitTextInBox_currentBox).width()*0.8;	
+        Drupal.fitTextInBox.maxHeight = jQuery(fitTextInBox_currentBox).parents(".circle-container").height()*0.8;	
+        Drupal.fitTextInBox.TextObjects = jQuery("span", fitTextInBox_currentBox);
+        Drupal.fitTextInBox.wordCount = Drupal.fitTextInBox.TextObjects.length;
+        
+        jQuery(Drupal.fitTextInBox.TextObjects).each(function(key ,fitTextInBox_currentTextObj){
+            jQuery(fitTextInBox_currentTextObj).addClass("fix");
+                    //.css("font-size","1px");
+            var fitTextInBox_currentWidth = jQuery(fitTextInBox_currentTextObj).width();
+            if(typeof(fitTextInBox_currentTextObj)!== "undefined"){
+                Drupal.fitTextInBox.wordHeight = parseInt(Drupal.fitTextInBox.maxHeight) / parseInt(Drupal.fitTextInBox.wordCount);
+               fitTextInBoxAutoFit(fitTextInBox_currentTextObj, Drupal.fitTextInBox.maxWidth ,fitTextInBox_currentWidth );
+            };
+        });
+	}	
+	
+	function fitTextInBoxAutoFit(fitTextInBox_currentTextObj, fitTextInBox_maxWidth ,fitTextInBox_currentWidth, fitTextInBox_maxHeight,fitTextInBox_wordHeight  ){
+            	if(typeof(fitTextInBox_currentTextObj)!== "undefined"){
+                    var tmpFontSize = jQuery(fitTextInBox_currentTextObj).css("font-size").replace('px','')/1;
+                    var fontsize = tmpFontSize + 1 + 'px';
+                    jQuery(fitTextInBox_currentTextObj).css("font-size", fontsize);
+                    var tmpWidth = jQuery(fitTextInBox_currentTextObj).width();
+                    var tmpHeight  = jQuery(fitTextInBox_currentTextObj).height();
+                    if(tmpWidth>=fitTextInBox_currentWidth && tmpWidth<fitTextInBox_maxWidth && tmpHeight<Drupal.fitTextInBox.wordHeight  && tmpFontSize<400){		
+                            fitTextInBox_currentWidth = tmpWidth;	
+                           fitTextInBoxAutoFit(fitTextInBox_currentTextObj, fitTextInBox_maxWidth ,fitTextInBox_currentWidth, fitTextInBox_maxHeight);
+                    }else{
+                            var finalfontSize = fontsize.replace('px','')/1 - 1 + 'px';
+                            jQuery(fitTextInBox_currentTextObj).css("font-size", finalfontSize );
+                    }		
+                }
+        }
--- sites/all/themes/sub_bartik/templates/html--node--71.tpl.php
+++ sites/all/themes/sub_bartik/templates/html--node--71.tpl.php
@@ -72,6 +72,16 @@
     font-weight: normal;
     font-style: normal;
 }
+    @font-face {
+    font-family: 'avgardm';
+    src: url('/sites/default/files/fonts/AVGARDM.eot');
+    src: url('/sites/default/files/fonts/AVGARDM.eot?#iefix') format('embedded-opentype'),
+         url('/sites/default/files/fonts/AVGARDM.woff') format('woff'),
+         url('/sites/default/files/fonts/AVGARDM.ttf') format('truetype'),
+         url('/sites/default/files/fonts/AVGARDM.svg#alefregular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+}
 *{
     outline: none;
     font-family: Alef;
@@ -83,6 +93,7 @@ body.admin-menu{
     margin-top: 0px !important;
     background-color: black;
     color: white;
+    background-image: url("/bg_pepole.jpg");
 }
 
 .graphLabel {
@@ -128,6 +139,8 @@ body.admin-menu{
     text-align: center;
     top: -680px;
     width: 100%;
+    color: rgb(51, 204, 204); 
+    font-family: avgardm;
 }
 
 #counter span {
@@ -139,24 +152,201 @@ body.admin-menu{
     top:250px;
 }
 #logo{
-      background: url("/jukespot-logo.png") no-repeat scroll center center transparent;
+       background: url("/jukespot-logo.png") no-repeat scroll center center transparent;
     border-bottom: 1px solid;
     border-top: 1px solid white;
     display: block;
+    height: auto !important;
     margin: 10px 5%;
     top: 0;
+    width: 90% !important;
+}
+/*dream effect -- START*/
+.drawingpix	{ 
+
+	position:absolute; 
+
+	top:-50px; 
+
+	left:-50px;
+
+}
+
+
+
+a{
+
+text-decoration:none;
+
+/*color:#0075FF;*/
+
+z-index:1;
+
+}
+
+a div{
+
+display:none;
+
+position: absolute;
+
+margin-top:105px;
+
+margin-left:133px;
+
+}
+
+a:hover div{
+
+display:block;
+
+}
+
+.tip {
+
+ padding:10px;
+
+position:absolute;
+
+top:0%;
+
+left:0%;
+
+width:300px;
+
+height:300px;
+
+margin-left:0px;
+
+margin-top:0px;
+
+text-align:center;
+
+
+
+  opacity:0.8;
+
+  filter:alpha(opacity=80);
+
+}
+
+
+
+.tip .body {
+
+  color: #0075FF;
+
+  background-color: #B5FF00;
+
+  padding:5px 5px 5px 5px;
+
+   border-radius:5px; 
+
+  -moz-border-radius:5px;
+
+  -webkit-border-radius:5px;
+
+}
+/*dream effect -- END */
+/*  circle    */
+.circle{
+    border-radius:	100% !important;
+    -moz-border-radius:  100% !important;
+    -webkit-border-radius: 500px  !important;
+    /*background: none repeat scroll 0 0 #ffffff;*/
+    border-radius: 100% 100% 100% 100% !important;
+    border-style: solid;
+    border-width: 5px;
+    float: right;
+    height: 88%;
+    margin: 1%;
+    width: 88%;
+    text-align: center;
+    vertical-align: middle !important;   
+    /*font-size: 300%;*/
+    cursor: pointer;
+    padding:5%;    
+      display: none;
+}
+body.page .circle-container{
+    width: 100% !important;height: 100% !important;
+}
+.circle-container{
+    padding: 0px;margin: 0px;position: absolute;z-index: 10;
+}
+#container {
+ display: block;
+margin-bottom: 15px;
+margin-left: auto;
+margin-right: auto;
+position: relative;
+top: 70px;
+}
+span.word {
+    font-weight: 900;
+}
+@-webkit-keyframes pulseScale{
+0%{        opacity: 1;         }
+25%{        opacity: .5;    }
+50%{        opacity: .1;    }
+75%{        opacity: .8;    }
+100%{      opacity: 1;         }
+}
+@-moz-keyframes pulseScale{
+0%{        opacity: 1;         }
+25%{        opacity: .5;    }
+50%{        opacity: .1;    }
+75%{        opacity: .8;    }
+100%{      opacity: 1;         }
+}
+.pulse{
+animation-delay:0;
+ animation-duration: 0.5s;
+    animation-iteration-count: 17;
+    animation-name: pulseScale;
+ -webkit-animation-duration: 0.5s;
+    -webkit-animation-iteration-count: 5;
+    -webkit-animation-name: pulseScale;
+}
+
+/*  circle  -- ENd  */
+#countdown {
+    color: rgb(51, 204, 204); 
+    position: fixed;
+    top: 100px;
+    text-align: center;
     width: 90%;
+    font-size: 200px;
+       font-weight: 900;
+}
+.kkc-godz-text,
+.kkc-godz{
+    display:none;
+}
+#time_left_text{
+    font-size: 60px ;
+       font-weight: 900;
+}
+.kkcount-down * {
+  font-family: avgardm !important;
 }
 </style>
 
 
 <link rel="text/css" type="text/css" media="all" src="/sites/all/themes/sub_bartik/jukespot.css"/>
-<script type="text/javascript" src="/sites/all/modules/mimi/mimi.js" type="text/javascript"></script>
+<!--<script type="text/javascript" src="/sites/all/modules/mimi/mimi.js" type="text/javascript"></script>-->
     <script type="text/javascript" src="/sites/all/modules/mimi/results.js" type="text/javascript"></script>
     <script type="text/javascript" src="/sites/all/modules/mimi/jqBarGraph.1.1.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="/sites/all/modules/mimi/kkcountdown.js" type="text/javascript"></script>
 </head>
 <body class="multi">
     <div id="logo"></div>
+    <div id="container" class="">
+    
+    <div class="circle-container " id="a">                <div class="circle ">
+            <a href="#" class="circle_link " id="circle_1"><span></span></a>
+    </div>            </div>
+</div>
 <?php  print $page; ?>
 </body>
 </html>
